8651d1d905314d472e4d62c57829570e55e556ba
sanasham <sayyedesubvali@gmail.com>
2023-07-25 23:35:22 +0530

    authentication added with login and signup

--- todo-list/authentication/authController.js
+++ todo-list/authentication/authController.js
@@ -1,5 +1,11 @@
 const userInfo = (req, res) => {
   res.send('user route is displaying data from controller');
 };
+const userSignup = (req, res) => {
+  res.send('user route is signup');
+};
+const userLogin = (req, res) => {
+  res.send('user route for login');
+};
 
-module.exports = { userInfo };
+module.exports = { userInfo, userSignup, userLogin };
--- todo-list/authentication/authRoutes.js
+++ todo-list/authentication/authRoutes.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('./authController.js');
 router.get('/', userController.userInfo);
+router.post('/signup', userController.signUp);
+router.post('/login', userController.login);
 module.exports = router;
